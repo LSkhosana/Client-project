1a866fb937ec18014e7ea042df089749f6d0d517
Lesedi <skhosanalesedi888@gmail.com>
2024-10-03 15:43:58 +0200

    layout update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,11 @@
 import {BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
-import Form from "./Pages/Form";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/form" element={<Form/>} />
       </Routes>
   </BrowserRouter>
   );
---
+++ frontend/src/Components/Hero.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import './home.css'
+import btn from '../Assets/icon.png'
+import hero from '../Assets/img.png'
+
+const Hero = () => {
+  return (
+    <>
+    <section className='hero'>
+        <div className='head'>
+            <div className='st'>
+                <p id="h1">Mocaby & Co <br/>Group</p>
+                <p id="p1">Management consultants</p>
+                <p id="p2">
+                    We help businesses thrive with personalized consulting services, 
+                    strategic planning, and operational expertise. Trust our team to drive growth,
+                    improve profitability, and overcome challenges.
+                </p>
+                <div className='btns'>
+                    <button className='start'>Get Started</button>
+                    <div className='learn'>
+                        <img src={btn} alt='learn more'/>
+                        <p>Learn More</p>
+                    </div>
+                </div>
+            </div>
+            <div className='hero-img'>
+                <img src={hero} alt='hero'/>
+            </div>
+        </div>
+        
+     </section>
+
+    </>
+  )
+}
+
+export default Hero
